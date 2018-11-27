b7ab79d2329b7ec76f503d7406fd07ae69f7130c
Ben Kaufman <ben.kaufman10@gmail.com>
2018-11-27 15:45:43 +0200

    Simpler pruning and spinner

--- pruneArcBuild.js
+++ pruneArcBuild.js
@@ -1,27 +1,28 @@
 const glob = require("glob");
 var fs = require("fs");
+const ora = require("ora");
 
 const files = glob.sync("./node_modules/@daostack/arc/build/contracts/*.json", {
   nodir: true
 });
 
-console.log(`Starts pruning Arc JSON files`);
+const spinner = ora();
+spinner.info(`Starts pruning Arc JSON files`);
 
 files.filter(file => {
-  const contract = require(`${file}`);
-  console.log(`Pruning ${contract.contractName}`);
-  var cleanVersion = '{\n "contractName": "';
-  cleanVersion += contract.contractName;
-  cleanVersion += '",\n "abi": ';
-  cleanVersion += JSON.stringify(contract.abi);
-  cleanVersion += ',\n "bytecode": "';
-  cleanVersion += contract.bytecode;
-  cleanVersion += '",\n "deployedBytecode": "';
-  cleanVersion += contract.deployedBytecode;
-  cleanVersion += '" \n}';
-  fs.writeFile(file, cleanVersion, function(err, result) {
-    if (err) console.log("error", err);
-  });
+  const { contractName, abi, bytecode, deployedBytecode } = require(`${file}`);
+
+  spinner.info(`Pruning ${contractName}`);
+
+  fs.writeFileSync(
+    file,
+    JSON.stringify(
+      { contractName, abi, bytecode, deployedBytecode },
+      undefined,
+      2
+    ),
+    "utf-8"
+  );
 });
 
-console.log(`Finished pruning Arc JSON files`);
+spinner.succeed(`Finished pruning Arc JSON files`);
