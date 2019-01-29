61215c4bcbc37fdab81154df5b28a2e9989d345e
Ben Kaufman <ben.kaufman10@gmail.com>
2019-01-29 12:29:47 +0200

    release 0.0.1-rc.8-v1

--- prepare-release.sh
+++ prepare-release.sh
@@ -21,7 +21,7 @@ echo "Migrating rinkeby..."
 npm run migrate -- --gasPrice 10 --provider $rinkeby_provider --private-key $rinkeby_private_key "$@"
 # migrate mainnet
 echo "Migrating mainnet..."
-npm run migrate -- --gasPrice 5 --provider $mainnet_provider --private-key $mainnet_private_key "$@"
+npm run migrate -- --gasPrice 25 --provider $mainnet_provider --private-key $mainnet_private_key "$@"
 # set version
 echo "Setting version..."
 node set-version.js
