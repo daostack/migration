4b9af89191b6ee4eb0597b812376a1aa58b6eb3d
Ben Kaufman <ben.kaufman10@gmail.com>
2019-03-07 15:49:52 +0200

    .

--- prepare-release.sh
+++ prepare-release.sh
@@ -4,43 +4,43 @@ set -e
 source .env
 
 
-# # npm ci
-# echo "Installing NPM modules..."
-# npm install
 # npm ci
-# # recompiling contracts
-# echo "Recompiling contracts..."
-# npm explore @daostack/arc -- npm i
-# npm explore @daostack/arc -- npm run build
-# # generate abis
-# echo "Generating abis..."
-# npm run generate-abis
-# # prune arc build
-# echo "Pruning Arc build..."
-# npm run prune-arc-build -- "$@"
-# # migrate ganache
-# echo "Migrating ganache..."
-# npm run migrate -- "$@"
-# # migrate kovan
-# echo "Migrating kovan..."
-# npm run migrate -- --gasPrice 10 --provider $kovan_provider --private-key $kovan_private_key "$@"
-# # migrate rinkeby
-# echo "Migrating rinkeby..."
-# npm run migrate -- --gasPrice 10 --provider $rinkeby_provider --private-key $rinkeby_private_key "$@"
+echo "Installing NPM modules..."
+npm install
+npm ci
+# recompiling contracts
+echo "Recompiling contracts..."
+npm explore @daostack/arc -- npm i
+npm explore @daostack/arc -- npm run build
+# generate abis
+echo "Generating abis..."
+npm run generate-abis
+# prune arc build
+echo "Pruning Arc build..."
+npm run prune-arc-build -- "$@"
+# migrate ganache
+echo "Migrating ganache..."
+npm run migrate -- "$@"
+# migrate kovan
+echo "Migrating kovan..."
+npm run migrate -- --gasPrice 10 --provider $kovan_provider --private-key $kovan_private_key "$@"
+# migrate rinkeby
+echo "Migrating rinkeby..."
+npm run migrate -- --gasPrice 10 --provider $rinkeby_provider --private-key $rinkeby_private_key "$@"
 # migrate mainnet
 echo "Migrating mainnet..."
 npm run migrate -- --gasPrice 30 --provider $mainnet_provider --private-key $mainnet_private_key "$@"
 # set version
 echo "Setting version..."
 node set-version.js
+# update npm package lock
+echo "Updating package-lock..."
+npm install
 # commit addresses
 echo "Commiting changes..."
 git add -A && git commit -m "release $(cat package.json | jq -r '.version')"
 # push to git
 echo "Pushing to github..."
 git push -f
-# update npm package lock
-echo "Updating package-lock..."
-npm install
 # done
 echo "Done!"
