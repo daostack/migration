d52c6a2d7adf0a436343954c89fbef1f958e7c56
Ben <ben.kaufman10@gmail.com>
2019-09-18 10:10:58 +0300

    Update prepare-release.sh

--- prepare-release.sh
+++ prepare-release.sh
@@ -3,45 +3,45 @@
 set -e
 source .env
 
-# # npm ci
-# echo "Installing NPM modules..."
-# # best to start from sratch or the "verify" script may fail
-# rm -r node_modules
-# rm package-lock.json
-# npm install
 # npm ci
-# # initial Arc vrsion to use
-# readonly INITIAL_VERSION=16
-# # get latest Arc version
-# readonly ARC=$(cat package.json | jq -r '.dependencies."@daostack/arc"' | rev | cut -d'.' -f 1 | rev)
-# # migrate ganache
-# for (( version=$INITIAL_VERSION; version<=$ARC; version++ ))
-# do
-# if [ "$version" == "20" ]; then
-# continue
-# fi
-# echo "Installing Arc version $version..."
-# npm install "@daostack/arc@0.0.1-rc.$version"
-# # generate abis
-# echo "Generating abis..."
-# npm run generate-abis
-# # prune arc build
-# echo "Pruning Arc build..."
-# npm run prune-arc-build -- "$@"
-# # migrating Arc version to ganache
-# echo "Migrating ganache..."
-# npm run migrate -- --force "$@"
-# done
-# if [ ! -z "$kovan_private_key" ]; then
-# # migrate kovan
-# echo "Migrating kovan..."
-# npm run migrate -- --gasPrice 10 --provider $kovan_provider --private-key $kovan_private_key "$@"
-# fi
-# if [ ! -z "$rinkeby_private_key" ]; then
-# # migrate rinkeby
-# echo "Migrating rinkeby..."
-# npm run migrate -- --gasPrice 10 --provider $rinkeby_provider --private-key $rinkeby_private_key "$@"
-# fi
+echo "Installing NPM modules..."
+# best to start from sratch or the "verify" script may fail
+rm -r node_modules
+rm package-lock.json
+npm install
+npm ci
+# initial Arc vrsion to use
+readonly INITIAL_VERSION=16
+# get latest Arc version
+readonly ARC=$(cat package.json | jq -r '.dependencies."@daostack/arc"' | rev | cut -d'.' -f 1 | rev)
+# migrate ganache
+for (( version=$INITIAL_VERSION; version<=$ARC; version++ ))
+do
+if [ "$version" == "20" ]; then
+continue
+fi
+echo "Installing Arc version $version..."
+npm install "@daostack/arc@0.0.1-rc.$version"
+# generate abis
+echo "Generating abis..."
+npm run generate-abis
+# prune arc build
+echo "Pruning Arc build..."
+npm run prune-arc-build -- "$@"
+# migrating Arc version to ganache
+echo "Migrating ganache..."
+npm run migrate -- --force "$@"
+done
+if [ ! -z "$kovan_private_key" ]; then
+# migrate kovan
+echo "Migrating kovan..."
+npm run migrate -- --gasPrice 10 --provider $kovan_provider --private-key $kovan_private_key "$@"
+fi
+if [ ! -z "$rinkeby_private_key" ]; then
+# migrate rinkeby
+echo "Migrating rinkeby..."
+npm run migrate -- --gasPrice 10 --provider $rinkeby_provider --private-key $rinkeby_private_key "$@"
+fi
 if [ ! -z "$mainnet_private_key" ]; then
 # migrate mainnet
 echo "Migrating mainnet..."
