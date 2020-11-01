d14aa7229bd01e7596f89b20c661d896dc84cf5a
benk10 <ben@daostack.io>
2020-11-01 15:38:45 +0200

    V8 experimental (#366)
    * Create experimental-dao-params-generator.js
    
    * release 0.1.2-rc.8-v0

--- prepare-release.sh
+++ prepare-release.sh
@@ -45,7 +45,7 @@ fi
 if [ ! -z "$mainnet_private_key" ]; then
 # migrate mainnet
 echo "Migrating mainnet..."
-# npm run migrate.base -- --disableconfs --gasPrice 55 --provider $mainnet_provider --private-key $mainnet_private_key "$@"
+npm run migrate.base -- --disableconfs --gasPrice 30 --provider $mainnet_provider --private-key $mainnet_private_key "$@"
 fi
 if [ ! -z "$xdai_private_key" ]; then
 # migrate xdai
